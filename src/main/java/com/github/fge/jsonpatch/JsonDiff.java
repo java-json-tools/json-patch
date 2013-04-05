14c7a1dc19d56e94854967b41ee6f4bc5ea79e48
Francis Galiegue <fgaliegue@gmail.com>
2013-04-05 16:15:51 +0200

    Naive root replace diff implementation

--- src/main/java/com/github/fge/jsonpatch/JsonDiff.java
+++ src/main/java/com/github/fge/jsonpatch/JsonDiff.java
@@ -1,8 +1,11 @@
 package com.github.fge.jsonpatch;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.fge.jackson.JacksonUtils;
+import com.github.fge.jackson.jsonpointer.JsonPointer;
 
 public final class JsonDiff
 {
@@ -14,6 +17,23 @@ public final class JsonDiff
 
     public static JsonNode asJson(final JsonNode first, final JsonNode second)
     {
-        return FACTORY.arrayNode();
+        final ArrayNode ret = FACTORY.arrayNode();
+
+        ObjectNode op;
+        if (!first.equals(second)) {
+            op = createOp("replace", JsonPointer.empty());
+            op.put("value", second.deepCopy());
+            ret.add(op);
+        }
+        return ret;
+    }
+
+    private static ObjectNode createOp(final String name,
+        final JsonPointer ptr)
+    {
+        final ObjectNode ret = FACTORY.objectNode();
+        ret.put("op", name);
+        ret.put("path", ptr.toString());
+        return ret;
     }
 }
