6990a724c257a1d9932bd752d5472e951de3566d
Francis Galiegue <fgaliegue@gmail.com>
2014-02-10 15:28:04 +0100

    Switch to gradle for build

--- dorelease.sh
+++ dorelease.sh
@@ -3,7 +3,6 @@
 #
 # This will build everything that is needed and push to Maven central.
 #
-# The only thing missing is making this script non interactive...
-#
 
-mvn clean javadoc:jar source:jar package gpg:sign repository:bundle-create deploy
+./gradlew --recompile-scripts clean test uploadArchives
+
