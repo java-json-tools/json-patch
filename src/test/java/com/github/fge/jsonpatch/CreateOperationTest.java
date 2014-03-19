35102dbc9796afd9a11ea8da32c7325688509309
Hieu Nguyen <hieu@box.com>
2014-03-19 15:52:16 -0700

    add "create" operation

---
+++ src/test/java/com/github/fge/jsonpatch/CreateOperationTest.java
@@ -0,0 +1,13 @@
+package com.github.fge.jsonpatch;
+
+import java.io.IOException;
+
+public final class CreateOperationTest
+    extends JsonPatchOperationTest
+{
+    public CreateOperationTest()
+        throws IOException
+    {
+        super("create");
+    }
+}
