9ecd0126b32043fc834ffa9e33a36a538cec8fe2
Francis Galiegue <fgaliegue@gmail.com>
2014-10-31 11:58:44 +0100

    RFC 7386, part 1: Jackson {de,}serialization tests
    Mostly works, except JSON null returns null instead of a new
    NonObjectMergePatch(NullNode)...

---
+++ src/main/java/com/github/fge/jsonpatch/rfc7386/JsonMergePatchDeserializer.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2014, Francis Galiegue (fgaliegue@gmail.com)
+ *
+ * This software is dual-licensed under:
+ *
+ * - the Lesser General Public License (LGPL) version 3.0 or, at your option, any
+ *   later version;
+ * - the Apache Software License (ASL) version 2.0.
+ *
+ * The text of this file and of both licenses is available at the root of this
+ * project or, if you have the jar distribution, in directory META-INF/, under
+ * the names LGPL-3.0.txt and ASL-2.0.txt respectively.
+ *
+ * Direct link to the sources:
+ *
+ * - LGPL 3.0: https://www.gnu.org/licenses/lgpl-3.0.txt
+ * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package com.github.fge.jsonpatch.rfc7386;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+
+public final class JsonMergePatchDeserializer
+    extends JsonDeserializer<JsonMergePatch>
+{
+    /**
+     * Method that can be called to ask implementation to deserialize
+     * JSON content into the value type this serializer handles.
+     * Returned instance is to be constructed by method itself.
+     * <p>
+     * Pre-condition for this method is that the parser points to the
+     * first event that is part of value to deserializer (and which
+     * is never JSON 'null' literal, more on this below): for simple
+     * types it may be the only value; and for structured types the
+     * Object start marker.
+     * Post-condition is that the parser will point to the last
+     * event that is part of deserialized value (or in case deserialization
+     * fails, event that was not recognized or usable, which may be
+     * the same event as the one it pointed to upon call).
+     * <p>
+     * Note that this method is never called for JSON null literal,
+     * and thus deserializers need (and should) not check for it.
+     *
+     * @param jp Parsed used for reading JSON content
+     * @param ctxt Context that can be used to access information about
+     * this deserialization activity.
+     * @return Deserializer value
+     */
+    @Override
+    public JsonMergePatch deserialize(final JsonParser jp,
+        final DeserializationContext ctxt)
+        throws IOException, JsonProcessingException
+    {
+        final JsonNode node = jp.readValueAs(JsonNode.class);
+        return new NonObjectMergePatch(node);
+    }
+}
---
+++ src/test/java/com/github/fge/jsonpatch/rfc7386/SerializationTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2014, Francis Galiegue (fgaliegue@gmail.com)
+ *
+ * This software is dual-licensed under:
+ *
+ * - the Lesser General Public License (LGPL) version 3.0 or, at your option, any
+ *   later version;
+ * - the Apache Software License (ASL) version 2.0.
+ *
+ * The text of this file and of both licenses is available at the root of this
+ * project or, if you have the jar distribution, in directory META-INF/, under
+ * the names LGPL-3.0.txt and ASL-2.0.txt respectively.
+ *
+ * Direct link to the sources:
+ *
+ * - LGPL 3.0: https://www.gnu.org/licenses/lgpl-3.0.txt
+ * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package com.github.fge.jsonpatch.rfc7386;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jackson.JacksonUtils;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jackson.JsonNumEquals;
+import com.google.common.base.Equivalence;
+import com.google.common.collect.Lists;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+public final class SerializationTest
+{
+    private static final Equivalence<JsonNode> EQUIVALENCE
+        = JsonNumEquals.getInstance();
+
+    private final ObjectMapper mapper = JacksonUtils.newMapper();
+    private final JsonNode testData;
+
+    public SerializationTest()
+        throws IOException
+    {
+        final String resource = "/jsonpatch/rfc7386/serialization.json";
+        testData = JsonLoader.fromResource(resource);
+    }
+
+    @DataProvider
+    public Iterator<Object[]> getInputs()
+    {
+        final List<Object[]> list = Lists.newArrayList();
+
+        for (final JsonNode node: testData)
+            list.add(new Object[] { node });
+
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "getInputs")
+    public void serializationAndBackWorksCorrectly(final JsonNode input)
+        throws IOException
+    {
+        final String in = input.toString();
+        final JsonMergePatch deserialized
+            = mapper.readValue(in, JsonMergePatch.class);
+
+        assertNotNull(deserialized);
+
+        final String out = mapper.writeValueAsString(deserialized);
+        final JsonNode serialized = JacksonUtils.getReader().readTree(out);
+
+        assertTrue(EQUIVALENCE.equivalent(input, serialized));
+    }
+}
