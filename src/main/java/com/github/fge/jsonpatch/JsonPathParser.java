e5b10008e19da5266ce78299e3f4e18b23b10c2d
Piotr Dytkowski <piotr.dytkowski@gmail.com>
2022-03-25 11:09:25 +0100

    Clearer exception for unsupported JPQ syntax

--- src/main/java/com/github/fge/jsonpatch/JsonPathParser.java
+++ src/main/java/com/github/fge/jsonpatch/JsonPathParser.java
@@ -1,20 +1,15 @@
 package com.github.fge.jsonpatch;
 
-import static com.github.fge.jsonpatch.JsonPatchOperation.BUNDLE;
-
 public class JsonPathParser {
 
     private static final String ARRAY_ELEMENT_REGEX = "(?<=\\.)(\\d+)";
 
     public static String tmfStringToJsonPath(String path) throws JsonPatchException {
-        if ("/".equals(path) || path.isEmpty()) {
-            return "$";
-        }
         if (path.startsWith("$")) {
             return path;
         }
         if (path.contains("?")) {
-            throw new JsonPatchException(BUNDLE.getMessage("jsonPatch.invalidPathExpression"));
+            throw new JsonPatchException("Invalid path, `?` are not allowed in JsonPointer expressions.");
         }
 
         return "$" + path.replace('/', '.').replaceAll(ARRAY_ELEMENT_REGEX, "[$1]");
